641d3581ee3e09251496887d986db703d8f17d77
Tom Hobbs <tomhobbs@outlook.com>
2024-02-16 22:07:48 +0000

    WIP broken :(

--- pages/index.js
+++ pages/index.js
@@ -1,69 +1,28 @@
-import Head from "next/head";
-import styles from "../styles/Home.module.css";
-import { useEffect, useState } from "react";
+import Head from "next/head"
+import styles from "../styles/Home.module.css"
+import { useEffect, useRef, useState } from "react"
 
 export default function Home() {
-  const [outputText, setOutputText] = useState("");
-  const [selections, setSelections] = useState([]);
-  const [inputContent, setInputContent] = useState("");
-  const [infos, setInfos] = useState([]);
-  const [reader, setReader] = useState({});
+  const [reader, setReader] = useState({})
+  const [inputContent, setInputContent] = useState("")
+  const [infos, setInfos] = useState([])
+  const [selections, setSelections] = useState([])
+  const [outputText, setOutputText] = useState("")
+  const dropZoneRef = useRef(null)
 
   useEffect(() => {
-    const reader = new FileReader();
-    reader.addEventListener("load", (e) => setInputContent(e.target.result));
-    setReader(reader);
+    const reader = new FileReader()
+    reader.addEventListener("load", (e) => setInputContent(e.target.result))
+    setReader(reader)
     if (window?.localStorage.getItem("selections")) {
-      setSelections(JSON.parse(window.localStorage.getItem("selections")));
+      setSelections(JSON.parse(window.localStorage.getItem("selections")))
       console.log(
         `Loaded ${selections.length} preferences from browser storage`
-      );
+      )
     }
-  }, []);
+  }, [])
 
   useEffect(() => {
-    insertOptions();
-    printOutput();
-  }, [inputContent]);
-
-  useEffect(() => {
-    printOutput();
-  }, [selections]);
-
-  const setInfosStorage = (selections) => {
-    const selctionsString = JSON.stringify(selections);
-    window.localStorage.setItem("selections", selctionsString);
-  };
-
-  const dropHandler = (ev) => {
-    ev.preventDefault();
-    if (ev.dataTransfer.items) {
-      [...ev.dataTransfer.items].forEach((item, i) => {
-        if (item.kind === "file") {
-          reader.readAsText(file);
-        }
-      });
-    }
-    // dropZone.style["border"] = "5px dashed lightgrey"
-  };
-
-  const dragOverHandler = (ev) => {
-    dropZone.style["border"] = "5px solid lightgrey";
-    ev.preventDefault();
-  };
-
-  const dragLeaveHandler = (ev) => {
-    dropZone.style["border"] = "5px dashed lightgrey";
-    ev.preventDefault();
-  };
-
-  const changeHandler = (event) => {
-    // setFile()
-    // console.log({ file })
-    reader.readAsText(event.target?.files?.[0]);
-  };
-
-  const insertOptions = () => {
     setInfos(
       [
         ...inputContent
@@ -73,35 +32,49 @@ export default function Home() {
       ].map((match) => {
         const infoFromStorage = selections.find(
           (sel) => sel.name === match?.[0].replace("\t", "")
-        );
+        )
         return {
           name: match?.[0].replace("\t", ""),
           before: infoFromStorage?.before || "",
           after: infoFromStorage?.after || "",
-        };
+        }
       })
-    );
-  };
+    )
+  }, [inputContent])
+
+  useEffect(() => {
+    printOutput()
+  }, [selections])
+
+  const setInfosStorage = (selections) => {
+    const selectionsString = JSON.stringify(selections)
+    window.localStorage.setItem("selections", selectionsString)
+  }
+
+  const changeHandler = (event) => {
+    reader.readAsText(event.target?.files?.[0])
+  }
 
   const handleDelimiterChange = (e) => {
     setSelections(
       selections.map((selection) => {
         if (e.target.id.includes(selection.name)) {
           if (e.target.id.includes("before")) {
-            selection.before = e.target.value;
+            selection.before = e.target.value
           }
           if (e.target.id.includes("after")) {
-            selection.after = e.target.value;
+            selection.after = e.target.value
           }
         }
-        return selection;
+        return selection
       })
-    );
-    setInfosStorage(selections);
-    printOutput();
-  };
+    )
+    setInfosStorage(selections)
+    printOutput()
+  }
 
   const printOutput = () => {
+    console.log("printOutput()")
     const lines = inputContent
       .split("\n")
       .map((line) =>
@@ -109,7 +82,10 @@ export default function Home() {
           .replace(/\t\t\t/g, "\tempty\tempty\t ")
           .replace(/\t\t/g, "\tempty\t")
       )
-      .filter((line, i) => i !== 0 && line !== "");
+      .filter((line, i) => i !== 0 && line !== "")
+
+    console.log({ lines })
+    console.log({ infos })
     const columns = selections.map(({ name, before, after }) =>
       lines.map((line) => ({
         name: [...line.matchAll(/[^\t|\n]+/g)]?.[
@@ -118,10 +94,9 @@ export default function Home() {
         before,
         after,
       }))
-    );
-    console.log({ columns });
-
-    const tracks = [];
+    )
+    console.log({ columns })
+    const tracks = []
     for (let trackIndex = 0; trackIndex < columns[0]?.length; trackIndex++) {
       tracks.push(
         columns
@@ -130,53 +105,79 @@ export default function Home() {
               `${col[trackIndex].before}${col[trackIndex].name}${col[trackIndex].after}`
           )
           .join("")
-      );
-      console.log({ tracks });
+      )
     }
-    setOutputText(tracks.join("\n"));
-  };
+    setOutputText(tracks.join("\n"))
+  }
   const copyClickHandler = () => {
-    if (document.selection) {
-      const range = document.body.createTextRange();
-      range.moveToElementText(output);
-      range.select().createTextRange();
-      document.execCommand("copy");
-    } else if (window.getSelection) {
-      const range = document.createRange();
-      range.selectNode(output);
-      window.getSelection().addRange(range);
-      document.execCommand("copy");
-      alert(`Output for ${lines.length + 1} tracks copied to clipboard.`);
-    }
-  };
+    // if (document.selection) {
+    //   const range = document.body.createTextRange()
+    //   range.moveToElementText(output)
+    //   range.select().createTextRange()
+    //   document.execCommand("copy")
+    // } else if (window.getSelection) {
+    //   const range = document.createRange()
+    //   range.selectNode(output)
+    //   window.getSelection().addRange(range)
+    //   document.execCommand("copy")
+    //   alert(`Output for ${lines.length + 1} tracks copied to clipboard.`)
+    // }
+  }
 
   return (
     <div className={styles.container}>
       <Head>
         <title>Track ID Me!</title>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel='icon' href='/favicon.ico' />
       </Head>
 
       <main>
         <div
           className={styles.dropZone}
-          id="drop_zone"
-          onDrop={dropHandler}
-          onDragOver={dragOverHandler}
-          onDragLeave={dragLeaveHandler}
+          id='drop_zone'
+          ref={dropZoneRef}
+          style={
+            outputText
+              ? {}
+              : {
+                  position: "absolute",
+                  top: "50%",
+                  left: "50%",
+                  transform: "translate(-50%,-50%)",
+                }
+          }
+          onDrop={(e) => {
+            e.preventDefault()
+            if (e.dataTransfer.items) {
+              ;[...e.dataTransfer.items].forEach((item) => {
+                const file = item.getAsFile()
+                if (item.kind === "file") {
+                  reader.readAsText(file)
+                }
+              })
+            }
+            dropZoneRef.current.style["border"] = "5px dashed lightgrey"
+          }}
+          onDragOver={(e) => {
+            e.preventDefault()
+            dropZoneRef.current.style["border"] = "5px solid lightgrey"
+          }}
+          onDragLeave={(e) => {
+            e.preventDefault()
+            dropZoneRef.current.style["border"] = "5px dashed lightgrey"
+          }}
         >
-          <p>
+          <p className={styles.manual}>
             Drag a
             <img
-              loading="lazy"
-              decoding="async"
-              src="https://rekordbox.com/wp-content/themes/rekordbox/assets/img/2022/common/logo/rekordbox.svg"
-              alt="rekordbox"
-              width="90"
-              height="13"
+              src='https://rekordbox.com/wp-content/themes/rekordbox/assets/img/2022/common/logo/rekordbox.svg'
+              alt='rekordbox'
+              width='90'
+              height='13'
+              className={styles.rekordbox}
             />
-            playlist .txt file <i>here</i> or attach a file.{" "}
-            <input type="file" id="input" onChange={changeHandler} />
+            playlist .txt file <i>here</i> or attach a file.
+            <input type='file' id='input' onChange={changeHandler} />
           </p>
         </div>
         <div>
@@ -186,10 +187,12 @@ export default function Home() {
                 id={`before_${name}`}
                 defaultValue={before}
                 onChange={handleDelimiterChange}
+                rows={1}
+                cols={3}
               />
               <input
                 id={name}
-                type="checkbox"
+                type='checkbox'
                 checked={selections.map((s) => s.name).includes(name)}
                 onChange={(e) => {
                   if (e.target.checked) {
@@ -197,14 +200,13 @@ export default function Home() {
                       selections.map((s) => s.name).includes(name)
                         ? selections.filter((s) => s.name !== name)
                         : [...selections, { name, after: "", before: "" }]
-                    );
+                    )
                   } else {
-                    setSelections(selections.filter((s) => s.name !== name));
+                    setSelections(selections.filter((s) => s.name !== name))
                   }
-                  console.log({ selections });
                   if (inputContent) {
-                    setInfosStorage(selections);
-                    printOutput();
+                    setInfosStorage(selections)
+                    printOutput()
                   }
                 }}
               />
@@ -213,21 +215,25 @@ export default function Home() {
                 id={`after_${name}`}
                 defaultValue={after}
                 onChange={handleDelimiterChange}
+                rows={1}
+                cols={3}
               />
             </div>
           ))}
         </div>
-        <div className={styles.result}>
-          <textarea
-            defaultValue={outputText}
-            onChange={(e) => setOutputText(e.target.value)}
-            rows={outputText.split(/\n/).length + 1}
-            cols={outputText
-              .split(/\n/)
-              .reduce((a, b) => (a.length > b.length ? a.length : b.length))}
-          ></textarea>
-          <button onClick={copyClickHandler}>Copy 📋</button>
-        </div>
+        {outputText ? (
+          <div className={styles.result}>
+            <textarea
+              defaultValue={outputText}
+              onChange={(e) => setOutputText(e.target.value)}
+              rows={outputText.split(/\n/).length + 1}
+              cols={outputText
+                .split(/\n/)
+                .reduce((a, b) => (a.length > b.length ? a.length : b.length))}
+            ></textarea>
+            <button onClick={copyClickHandler}>Copy 📋</button>
+          </div>
+        ) : null}
       </main>
 
       <style jsx global>{`
@@ -244,5 +250,5 @@ export default function Home() {
         }
       `}</style>
     </div>
-  );
+  )
 }
