08d18fe383bc69545f022baaeb45220402270c4b
Tom Hobbs <thomas.hobbs@bbc.com>
2024-02-19 16:50:55 +0000

    Image support

--- pages/index.js
+++ pages/index.js
@@ -1,40 +1,64 @@
-import Head from "next/head"
-import styles from "../styles/Home.module.css"
-import { useEffect, useRef, useState } from "react"
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+import { useEffect, useRef, useState } from 'react'
+
+const CANVAS_SIZE = 1000
 
 export default function Home() {
   const [reader, setReader] = useState({})
-  const [inputContent, setInputContent] = useState("")
+  const [bgImgReader, setBgImgReader] = useState({})
+  const [inputContent, setInputContent] = useState('')
+  const [inputFileName, setInputFileName] = useState('temp.png')
+  const [bgImg, setBgImg] = useState(null)
   const [options, setOptions] = useState([])
   const [selections, setSelections] = useState([])
-  const [outputText, setOutputText] = useState("")
+  const [outputText, setOutputText] = useState('')
   const dropZoneRef = useRef(null)
+  const canvasRef = useRef(null)
+  const [fontSize, setFontSize] = useState('16px')
+  const [fontColor, setFontColor] = useState('#000000')
+  const [bgColor, setBgColor] = useState('#FFFFFF')
 
   useEffect(() => {
     const reader = new FileReader()
-    reader.addEventListener("load", (e) => {
+    reader.addEventListener('load', (e) => {
       setInputContent(e.target.result)
       setOptions(
         [
           ...e.target.result
-            .split("\n")
+            .split('\n')
             .shift()
             .matchAll(/(.+?)(?:\t|\n)/g),
         ].map((match) => {
           console.log({ match })
           const infoFromStorage = selections.find(
-            (sel) => sel.name === match?.[0].replace("\t", "")
+            (sel) => sel.name === match?.[0].replace('\t', '')
           )
           return {
-            name: match?.[0].replace("\t", ""),
-            after: infoFromStorage?.after || "",
+            name: match?.[0].replace('\t', ''),
+            after: infoFromStorage?.after || '',
           }
         })
       )
     })
     setReader(reader)
-    if (window?.localStorage.getItem("selections")) {
-      setSelections(JSON.parse(window.localStorage.getItem("selections")))
+
+    const bgReader = new FileReader()
+    bgReader.addEventListener('load', (e) => {
+      let img = new Image()
+      img.onload = () => {
+        img.width = '400px'
+        img.height = '400px'
+        console.log({ img })
+        canvasRef?.current?.canvas?.getContext('2d')?.drawImage(img, 0, 0)
+      }
+      img.src = e.target.result
+      setBgImg(img)
+    })
+    setBgImgReader(bgReader)
+
+    if (window?.localStorage.getItem('selections')) {
+      setSelections(JSON.parse(window.localStorage.getItem('selections')))
       console.log(
         `Loaded ${selections.length} preferences from browser storage`
       )
@@ -45,18 +69,16 @@ export default function Home() {
     setOptions(
       [
         ...inputContent
-          .split("\n")
+          .split('\n')
           .shift()
           .matchAll(/(.+?)(?:\t|\n)/g),
       ].map((match) => {
-        console.log({ match })
         const infoFromStorage = selections.find(
-          (sel) => sel.name === match?.[0].replace("\t", "")
+          (sel) => sel.name === match?.[0].replace('\t', '')
         )
         return {
-          name: match?.[0].replace("\t", ""),
-          // before: infoFromStorage?.before || "",
-          after: infoFromStorage?.after || "",
+          name: match?.[0].replace('\t', ''),
+          after: infoFromStorage?.after || '',
         }
       })
     )
@@ -68,23 +90,28 @@ export default function Home() {
     printOutput()
   }, [selections])
 
+  useEffect(() => {
+    draw()
+  }, [fontSize, fontColor, bgColor])
+
   const setSelectionsStorage = (selections) => {
     const selectionsString = JSON.stringify(selections)
-    window.localStorage.setItem("selections", selectionsString)
+    window.localStorage.setItem('selections', selectionsString)
   }
 
   const changeHandler = (event) => {
     reader.readAsText(event.target?.files?.[0])
+    setInputFileName(event.target?.files?.[0]?.name)
   }
 
   const handleDelimiterChange = (e) => {
     setSelections(
       selections.map((selection) => {
         if (e.target.id.includes(selection.name)) {
-          if (e.target.id.includes("before")) {
+          if (e.target.id.includes('before')) {
             selection.before = e.target.value
           }
-          if (e.target.id.includes("after")) {
+          if (e.target.id.includes('after')) {
             selection.after = e.target.value
           }
         }
@@ -96,18 +123,15 @@ export default function Home() {
   }
 
   const printOutput = () => {
-    console.log("printOutput()")
     const lines = inputContent
-      .split("\n")
+      .split('\n')
       .map((line) =>
         line
-          .replace(/\t\t\t/g, "\tempty\tempty\t ")
-          .replace(/\t\t/g, "\tempty\t")
+          .replace(/\t\t\t/g, '\tempty\tempty\t ')
+          .replace(/\t\t/g, '\tempty\t')
       )
-      .filter((line, i) => i !== 0 && line !== "")
+      .filter((line, i) => i !== 0 && line !== '')
 
-    // console.log({ lines })
-    console.log({ options })
     const columns = selections.map(({ name, before, after }) =>
       lines.map((line) => ({
         name: [...line.matchAll(/[^\t|\n]+/g)]?.[
@@ -117,7 +141,6 @@ export default function Home() {
         after,
       }))
     )
-    console.log({ columns })
     const tracks = []
     for (let trackIndex = 0; trackIndex < columns[0]?.length; trackIndex++) {
       tracks.push(
@@ -126,45 +149,68 @@ export default function Home() {
             (col) =>
               `${col[trackIndex].before}${col[trackIndex].name}${col[trackIndex].after}`
           )
-          .join("")
+          .join('')
       )
     }
-    // console.log({ tracks })
-    setOutputText(tracks.join("\n"))
+    setOutputText(tracks.join('\n'))
   }
   const copyClickHandler = async () => {
-    if ("clipboard" in navigator) {
+    if ('clipboard' in navigator) {
       await navigator.clipboard.writeText(outputText)
     } else {
-      document.execCommand("copy", true, outputText)
+      document.execCommand('copy', true, outputText)
     }
     alert(
       `Output for ${
-        outputText.split("\n").length + 1
+        outputText.split('\n').length + 1
       } tracks copied to clipboard.`
     )
   }
 
+  const draw = () => {
+    if (canvasRef.current) {
+      const canvas = canvasRef.current
+      const ctx = canvas.getContext('2d')
+      ctx.clearRect(0, 0, CANVAS_SIZE, CANVAS_SIZE)
+      ctx.fillStyle = bgColor
+      ctx.fillRect(0, 0, canvas.width, canvas.height)
+      bgImg && ctx.drawImage(bgImg, 0, 0, CANVAS_SIZE, CANVAS_SIZE)
+      const lines = outputText.split('\n')
+      const lineHeight = CANVAS_SIZE / lines.length
+      ctx.font = `${fontSize} serif`
+      ctx.fillStyle = fontColor
+      outputText.split('\n').map((line, index) => {
+        ctx.fillText(
+          line,
+          10,
+          index * lineHeight + Number(fontSize.replace('px', '')),
+          CANVAS_SIZE
+        )
+      })
+    }
+  }
+
+  draw()
   return (
     <div className={styles.container}>
       <Head>
         <title>Track ID Me!</title>
-        <link rel='icon' href='/favicon.ico' />
+        <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
         <div
           className={styles.dropZone}
-          id='drop_zone'
+          id="drop_zone"
           ref={dropZoneRef}
           style={
             inputContent
               ? {}
               : {
-                  position: "absolute",
-                  top: "50%",
-                  left: "50%",
-                  transform: "translate(-50%,-50%)",
+                  position: 'absolute',
+                  top: '50%',
+                  left: '50%',
+                  transform: 'translate(-50%,-50%)',
                 }
           }
           onDrop={(e) => {
@@ -172,33 +218,38 @@ export default function Home() {
             if (e.dataTransfer.items) {
               ;[...e.dataTransfer.items].forEach((item) => {
                 const file = item.getAsFile()
-                if (item.kind === "file") {
+                if (item.kind === 'file') {
                   reader.readAsText(file)
                 }
               })
             }
-            dropZoneRef.current.style["border"] = "5px dashed lightgrey"
+            dropZoneRef.current.style['border'] = '5px dashed lightgrey'
           }}
           onDragOver={(e) => {
             e.preventDefault()
-            dropZoneRef.current.style["border"] = "5px solid lightgrey"
+            dropZoneRef.current.style['border'] = '5px solid lightgrey'
           }}
           onDragLeave={(e) => {
             e.preventDefault()
-            dropZoneRef.current.style["border"] = "5px dashed lightgrey"
+            dropZoneRef.current.style['border'] = '5px dashed lightgrey'
           }}
         >
           <p className={styles.manual}>
             Drag a
             <img
-              src='https://rekordbox.com/wp-content/themes/rekordbox/assets/img/2022/common/logo/rekordbox.svg'
-              alt='rekordbox'
-              width='90'
-              height='13'
+              src="https://rekordbox.com/wp-content/themes/rekordbox/assets/img/2022/common/logo/rekordbox.svg"
+              alt="rekordbox"
+              width="90"
+              height="13"
               className={styles.rekordbox}
             />
             playlist .txt file <i>here</i> or attach a file.
-            <input type='file' id='input' onChange={changeHandler} />
+            <input
+              type="file"
+              id="input"
+              accept=".txt"
+              onChange={changeHandler}
+            />
           </p>
         </div>
         <div>
@@ -206,14 +257,14 @@ export default function Home() {
             <span key={`${index} ${name}`}>
               <input
                 id={name}
-                type='checkbox'
+                type="checkbox"
                 checked={selections.map((s) => s.name).includes(name)}
                 onChange={(e) => {
                   if (e.target.checked) {
                     setSelections(
                       selections.map((s) => s.name).includes(name)
                         ? selections.filter((s) => s.name !== name)
-                        : [...selections, { name, after: "", before: "" }]
+                        : [...selections, { name, after: '', before: '' }]
                     )
                   } else {
                     setSelections(selections.filter((s) => s.name !== name))
@@ -238,10 +289,61 @@ export default function Home() {
                 defaultValue={outputText}
                 onChange={(e) => setOutputText(e.target.value)}
                 rows={outputText.split(/\n/).length}
-                style={{ width: "100%" }}
+                style={{ width: '100%' }}
               />
             </div>
-            <button onClick={copyClickHandler}>Copy 📋</button>
+            <button onClick={copyClickHandler}>
+              Copy Text to Clipboard 📋
+            </button>
+            <label htmlFor="fontsize">Font size (px): </label>
+            <input
+              id="fontsize"
+              type="number"
+              defaultValue={Number(fontSize.replace('px', ''))}
+              onChange={(e) => setFontSize(`${e.target.value}px`)}
+              style={{ width: '45px' }}
+            />
+            <span>
+              <label for="fontColor">Font Colour</label>
+              <input
+                type="color"
+                id="fontColor"
+                name="head"
+                value={fontColor}
+                onChange={(e) => setFontColor(e.target.value)}
+              />
+            </span>
+            <span>
+              <label for="bgColor">Background Colour</label>
+              <input
+                type="color"
+                id="bgColor"
+                name="head"
+                value={bgColor}
+                onChange={(e) => setBgColor(e.target.value)}
+              />
+            </span>
+            <label htmlFor="bgImg">Background Image:</label>
+            <input
+              type="file"
+              id="bgImg"
+              accept=".jpg,.png"
+              onChange={(event) => {
+                bgImgReader.readAsDataURL(event.target?.files?.[0])
+              }}
+            />
+            <a
+              download={inputFileName?.replace('.txt', '.png')}
+              href={canvasRef?.current?.toDataURL('image/png')}
+            >
+              Save as Image 🖼️
+            </a>
+            <canvas
+              width={1000}
+              height={1000}
+              ref={canvasRef}
+              className={styles.canvas}
+            ></canvas>
           </div>
         ) : null}
       </main>
